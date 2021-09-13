e829b47fdc6ce22dc00788ecea2598da96b3c843
Joe <joe@xxtime.com>
2021-09-13 22:51:17 +0800

    utils path

---
+++ app/utils/path.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	defaultDirName = ".ZLabDrive/"
+)
+
+func HomeDir() string {
+	if os.Getenv("DIR_HOME") == "" {
+		return os.Getenv("HOME") + "/" + defaultDirName
+	}
+	return strings.TrimRight(os.Getenv("DIR_HOME"), "/") + "/"
+}
+
+func WorkDir(name string) string {
+	dir := HomeDir() + strings.Trim(name, "/") + "/"
+	if _, err := os.Stat(dir); err != nil {
+		os.MkdirAll(dir, 0700)
+	}
+	return dir
+}
+
+func TempDir() string {
+	dir := WorkDir("temp")
+	return dir
+}
+
+func NewLogName(name string, version string) string {
+	// 1610706262_2021_01_15_0.8.2-xx_data.txt
+	now := time.Now()
+	return strconv.FormatInt(now.Unix(), 10) + now.Format("_2006_01_02_") + version + "-" + name
+}
