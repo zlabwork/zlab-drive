6d41f6401869480d670bc625d684dabc6604707c
Joe <joe@xxtime.com>
2021-10-01 00:13:30 +0800

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,12 @@ let utilsBox = function () {
     let httpRequest = function (method, url, data) {
         // TODO :: configs
         axios.defaults.baseURL = 'http://localhost:8000/';
-        axios({
+        return axios({
             method: method,
             url: url,
             headers: {'content-type': 'application/x-www-form-urlencoded'},
             data: data
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+        });
     }
 
     return {
@@ -28,6 +19,7 @@ let utilsBox = function () {
 
 }()
 
+// 导航区
 class Breadcrumb extends React.Component {
     constructor(props) {
         super(props);
@@ -38,7 +30,7 @@ class Breadcrumb extends React.Component {
     }
 
     componentDidMount() {
-        console.log("execute right now")
+        // console.log("execute right now")
     }
 
     handleClick(name, event) {
@@ -67,7 +59,56 @@ class Breadcrumb extends React.Component {
     }
 }
 
+// 文件区
+class FileView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date(), files: []};
+
+        // 这边绑定是必要的，这样 `this` 才能在回调函数中使用
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        utilsBox.httpRequest("get", "/files/xxx?id=0").then((resp) => {
+            this.setState(prevState => ({
+                files: resp.data.data
+            }));
+        })
+    }
+
+    handleClick(id, event) {
+        console.log("click" + id)
+    }
+
+    handleDoubleClick(uuid, event) {
+        utilsBox.httpRequest("get", "/files/" + uuid).then((resp) => {
+            this.setState(prevState => ({
+                files: resp.data.data
+            }));
+        })
+    }
+
+    render() {
+        const element = this.state.files.map((item) =>
+            <figure key={item.id} className="figure"
+                    onClick={this.handleClick.bind(this, item.id)}
+                    onDoubleClick={this.handleDoubleClick.bind(this, item.uuid)}>
+                <img src="/holder/200x200" className="figure-img img-fluid rounded"/>
+                <figcaption className="figure-caption">{item.name}</figcaption>
+            </figure>
+        );
+        return <div className="col-md-12">{element}</div>
+    }
+}
+
+// 渲染
 ReactDOM.render(
     <Breadcrumb/>,
     document.getElementById('breadcrumb')
+);
+
+ReactDOM.render(
+    <FileView/>,
+    document.getElementById('fileView')
 );
