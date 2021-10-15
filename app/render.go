f8ec953cfbe0cdc15a8cbbd57d1c1fd30897be2c
Joe <joe@xxtime.com>
2021-10-16 00:15:36 +0800

    rename

--- app/render.go
+++ app/render.go
@@ -6,11 +6,11 @@ import (
 )
 
 func templateRender(w io.Writer, tmpl string, data interface{}) {
-	t, _ := template.ParseFiles("../templates/"+tmpl, "../templates/parts/header.html", "../templates/parts/footer.html", "../templates/parts/sidebar.html")
+	t, _ := template.ParseFiles("../template/"+tmpl, "../template/parts/header.html", "../template/parts/footer.html", "../template/parts/sidebar.html")
 	t.Execute(w, data)
 }
 
 func renderDrive(w io.Writer, tmpl string, data interface{}) {
-	t, _ := template.ParseFiles("../templates/"+tmpl, "../templates/parts/drive_header.html", "../templates/parts/drive_footer.html", "../templates/parts/drive_sidebar.html", "../templates/parts/drive_setting.html")
+	t, _ := template.ParseFiles("../template/"+tmpl, "../template/parts/drive_header.html", "../template/parts/drive_footer.html", "../template/parts/drive_sidebar.html", "../template/parts/drive_setting.html")
 	t.Execute(w, data)
 }
